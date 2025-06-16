2d51fb3e0a1f62f6b48058fc5defc25d7f5bcdb8
jkubicius <jkubicius@griddynamics.com>
2025-06-16 11:44:42 +0200

    Fixed according to the feedback task#1

--- practice/1_python_part_1/task1.py
+++ practice/1_python_part_1/task1.py
@@ -15,15 +15,14 @@ from typing import List, Any
 
 
 def delete_from_list(list_to_clean: List, item_to_delete: Any) -> List:
-    i = 0
-    while i < len(list_to_clean):
+    i = len(list_to_clean) - 1
+    while i >= 0:
         if list_to_clean[i] == item_to_delete:
             list_to_clean.pop(i)
-        else:
-            i += 1
+        i -= 1
     return list_to_clean
 
-# Code complexity is O(n^2)
+# Code complexity O(n)
 
 print(delete_from_list([1, 2, 3, 4, 3], 3)) # [1, 2, 4]
 print(delete_from_list(['a', 'b', 'c', 'b', 'd'], 'b'))  # ['a', 'c', 'd']
