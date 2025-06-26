c97c852c4a48f85ad9e1a5c266c18d2d90cc5755
jkubicius <jkubicius@griddynamics.com>
2025-06-26 11:22:36 +0200

    fix test_task_parametrize

--- practice/3_python_testing/test_task_parametrize.py
+++ practice/3_python_testing/test_task_parametrize.py
@@ -11,20 +11,18 @@ Task:
 """
 import pytest
 
-testdata = [
-    (1, 1),
-    (2, 1),
-    (10, 55),
-    (11, 89),
-    (12, 144),
-]
+testdata = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181]
+
+def fibonacci_expected():
+    return list(enumerate(testdata)) #generates (n, expected) pairs from the testdata list
 
 def fibonacci_1(n):
+    if n <= 0:
+        return 0
     a, b = 0, 1
     for _ in range(n-1):
         a, b = b, a + b
     return b
-
 
 def fibonacci_2(n):
     fibo = [0, 1]
@@ -32,12 +30,10 @@ def fibonacci_2(n):
         fibo.append(fibo[i-1] + fibo[i-2])
     return fibo[n]
 
+@pytest.mark.parametrize("test_input,expected", fibonacci_expected())
+def test_fibonacci_1(test_input, expected):
+    assert fibonacci_1(test_input) == expected
 
-@pytest.mark.parametrize("n,expected", testdata)
-def test_fibonacci_1(n, expected):
-    assert fibonacci_1(n) == expected
-
-
-@pytest.mark.parametrize("n,expected", testdata)
-def test_fibonacci_2(n, expected):
-    assert fibonacci_2(n) == expected
+@pytest.mark.parametrize("test_input,expected", fibonacci_expected())
+def test_fibonacci_2(test_input, expected):
+    assert fibonacci_2(test_input) == expected
