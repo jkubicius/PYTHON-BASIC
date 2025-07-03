e16f63f850f3a627c9aa45e0561f519d63607117
jkubicius <jkubicius@griddynamics.com>
2025-07-03 22:05:26 +0200

    tasks from part2 and part3

--- practice/2_python_part_2/task_read_write.py
+++ practice/2_python_part_2/task_read_write.py
@@ -5,11 +5,40 @@ Write one file with all values separated by commas.
 Example:
     Input:
 
-    file_1.txt (content: "23")
-    file_2.txt (content: "78")
-    file_3.txt (content: "3")
+    file_1.txt (content: "80")
+    file_2.txt (content: "37")
+    file_3.txt (content: "15")
 
     Output:
 
-    result.txt(content: "23, 78, 3")
+    result.txt(content: "80, 37, 15")
 """
+import os
+
+
+def read_files(file_paths: list[str]):
+    result = []
+    for file_path in file_paths:
+        try:
+            with open(file_path, 'r') as content:
+                result.append(content.read())
+        except FileNotFoundError:
+            print(f"Missing file: {file_path}")
+    return result
+
+def write_to_file(filename: str, content: list[str]):
+    with open(filename, 'w') as file:
+        file.write(', '.join(content)) # join list elements with ,
+
+def get_number(filename: str):
+    return int(filename.split('_')[1].split('.')[0]) # extract number from filename
+
+if __name__ == "__main__":
+
+    folder = './files'
+
+    files = sorted(os.listdir(folder), key=get_number)
+    files = [os.path.join(folder, filename) for filename in files]
+
+    content = read_files(files)
+    write_to_file("result.txt", content)
